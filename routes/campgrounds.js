b83725837fe8b0306e65c7c81a5b0ab6e6c86bf3
Sam Hudson <shudson44@live.com>
2020-09-13 16:28:14 +1000

    add search bar to landing page and index page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -20,14 +20,30 @@ const geocoder = NodeGeocoder(options);
 
 // INDEX - show all campgrounds
 router.get("/", function (req, res) {
-    // Get all campgrounds from DB
-    Campground.find({}, function (err, allCampgrounds) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("campgrounds/index", { campgrounds: allCampgrounds, page: 'campgrounds' });
-        }
-    });
+    if (req.query.search) {
+        var noMatch = null;
+        const regex = new RegExp(escapeRegex(req.query.search), "gi");
+        // Get all campgrounds from DB
+        Campground.find({ $or: [{ name: regex, }, { location: regex }, { "author.username": regex }] }, function (err, allCampgrounds) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (allCampgrounds.length < 1) {
+                    var noMatch = "No campgrounds match that query, please try again.";
+                }
+                res.render("campgrounds/index", { campgrounds: allCampgrounds, page: 'campgrounds', noMatch: noMatch });
+            };
+        });
+    } else {
+        // Get all campgrounds from DB
+        Campground.find({}, function (err, allCampgrounds) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("campgrounds/index", { campgrounds: allCampgrounds, page: 'campgrounds', noMatch: noMatch });
+            };
+        });
+    };
 });
 
 //CREATE - add new campground to DB
@@ -123,5 +139,8 @@ router.delete("/:id", middleware.checkCampgroundOwnership, function (req, res) {
     });
 });
 
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
 
 module.exports = router;
