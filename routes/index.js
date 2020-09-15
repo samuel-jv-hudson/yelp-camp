c56c42791bddb27fcdb6e6a9a18bad5e4cfdfeb8
Sam Hudson <shudson44@live.com>
2020-09-15 17:29:12 +1000

    still not working

--- routes/index.js
+++ routes/index.js
@@ -140,6 +140,7 @@ router.get('/reset/:token', function (req, res) {
     User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function (err, user) {
         if (!user) {
             req.flash('error', 'Password reset token is invalid or has expired.');
+            console.log("THIS IS THE ERROR: ", err);
             return res.redirect('/forgot');
         }
         res.render('reset', { token: req.params.token });
