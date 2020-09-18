77965b44bed8023a53ce84d229b58b6bbac97e43
Sam Hudson <shudson44@live.com>
2020-09-18 23:31:05 +1000

    first refactor to async await of seed.js

--- app.js
+++ app.js
@@ -40,7 +40,8 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-// SEED THE DATABASE // seedDB();
+// SEED THE DATABASE 
+// seedDB();
 
 // requiring moments.js to app
 app.locals.moment = require('moment');
--- seeds.js
+++ seeds.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose"),
     Campground = require("./models/campground"),
     Comment = require("./models/comment");
 
-const data = [
+const seeds = [
     {
         name: "Cloud's Rest",
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
@@ -18,40 +18,32 @@ const data = [
         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     }
-]
+];
 
-function seedDB() {
-    // REMOVE ALL CAMPGROUNDS
-    Campground.deleteMany({}, function (err) {
-        if (err) {
-            console.log(err);
+async function seedDB() {
+    try {
+        await Campground.deleteMany({});
+        console.log("Campgrounds removed");
+        await Comment.deleteMany({});
+        console.log("Comments removed");
+
+        for (const seed of seeds) {
+            let campground = await Campground.create(seed);
+            console.log("Campground created");
+            let comment = await Comment.create(
+                {
+                    text: "This palce is great but I wish there was internet",
+                    author: "Homer"
+                }
+            )
+            console.log("Comment created");
+            campground.comments.push(comment);
+            campground.save();
+            console.log("Comment added to campground");
         }
-        console.log("removed campgrounds!")
-        // add a few campgrounds
-        data.forEach(function (seed) {
-            Campground.create(seed, function (err, campground) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log("added campground!");
-                    // create a comment
-                    Comment.create({
-                        text: "This palce is great but I wish there was internet",
-                        author: "Homer"
-                    }, function (err, comment) {
-                        if (err) {
-                            console.log(err)
-                        } else {
-                            campground.comments.push(comment);
-                            campground.save();
-                            console.log("Created new comment");
-                        };
-                    });
-                };
-            });
-        });
-    });
-    // add a few comments
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 
